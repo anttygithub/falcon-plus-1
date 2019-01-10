acf70fdf605fbf6ff8b0d7027a08187a8a2bdb9b
anttygithub <763783130@qq.com>
2019-01-10 11:13:02 +0800

    bug fix

--- docker/confgen4docker.sh
+++ docker/confgen4docker.sh
@@ -14,6 +14,7 @@ confs=(
     '%%TRANSFER_RPC%%=0.0.0.0:8433'
     '%%PLUS_API_DEFAULT_TOKEN%%=default-token-used-in-server-side'
     '%%PLUS_API_HTTP%%=0.0.0.0:8080'
+    '%%TRANSFER_IMS_URL%%=http://0.0.0.0:10812/ims_data_access/send_machine_metric_by_json.do'
  )
 
 configurer() {
